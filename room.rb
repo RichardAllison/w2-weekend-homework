fd7b4ec7dd01c48c334a54117fe1421a3b4de7cb
Richard Allison <richard.allison.18@outlook.com>
2017-12-01 14:45:30 +0000

    created add song to playlist and check in guest functions in room.rb, passed tests

--- room.rb
+++ room.rb
@@ -1,10 +1,20 @@
 class Room
 
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :playlist, :guests
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @playlist = []
+    @guests = []
+  end
+
+  def add_song_to_playlist(song)
+    @playlist << song
+  end
+
+  def checkin_guest(guest)
+    @guests << guest
   end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,11 +7,24 @@ require_relative("../song.rb")
 class TestRoom < MiniTest::Test
 
   def setup
-    @room = Room.new("Big room", 100)
+    @big_room = Room.new("Big room", 50)
+    @small_room = Room.new("Small room", 10)
+    @guest = Guest.new("Richard Allison", 20)
+    @song = Song.new("Random song", "Random artist")
   end
 
   def test_room_has_name
-    assert_equal("Big room", @room.name)
+    assert_equal("Big room", @big_room.name)
+  end
+
+  def test_room_can_add_songs
+    @big_room.add_song_to_playlist(@song)
+    assert_equal(1, @big_room.playlist.length)
+  end
+
+  def test_room_can_checkin_guests
+    @big_room.checkin_guest(@guest)
+    assert_equal(1, @big_room.guests.length)
   end
 
 
